3cfa0e4e7a75e3eb6d3bb4cddf7089ab00694096
ouchulun <867333644@qq.com>
2026-01-20 10:16:32 +0800

    fix(*): 处理页面显示逻辑

--- src/themes/default/pages/dynamic-page.tsx
+++ src/themes/default/pages/dynamic-page.tsx
@@ -1,53 +1,55 @@
-import { getThemeBlock } from '@/core/theme';
-import type { DynamicPage as DynamicPageType } from '@/shared/types/blocks/landing';
+import { getThemeBlock } from '@/core/theme'
+import type { DynamicPage as DynamicPageType } from '@/shared/types/blocks/landing'
 
 export default async function DynamicPage({
   locale,
   page,
   data,
 }: {
-  locale?: string;
-  page: DynamicPageType;
-  data?: Record<string, any>;
-  }) {
-   
+  locale?: string
+  page: DynamicPageType
+  data?: Record<string, any>
+}) {
+  const sectionsList: any = page.show_sections?.length
+    ? page.show_sections
+    : (page.sections ? Object.keys(page.sections) : [])
   return (
-
     <>
       {page.title && !page.sections?.hero && (
         <h1 className="sr-only">{page.title}</h1>
       )}
-      {page?.show_sections &&
-        page.show_sections.map(async (sectionKey: string) => {
-          const section = page.sections?.[sectionKey];
-          if (!section || section.disabled === true) {
-            return null;
-          }
-          // block name
-          const block = section.block || section.id || sectionKey;
-          switch (block) {
-            default:
-              try {
-                
-                if (section.component) {
-                  return section.component;
-                }
+      {sectionsList.map(async (sectionKey: string) => {
+        const section = page.sections?.[sectionKey]
+        // console.log('dynamic-page :>> ', sectionKey, section)
+        if (!section || section.disabled === true) {
+          return null
+        }
 
-                const DynamicBlock = await getThemeBlock(block);
-     
-                // console.log('333 :>> ', sectionKey);
-                return (
-                  <DynamicBlock
-                    key={sectionKey}
-                    section={section}
-                    {...(data || section.data || {})}
-                  />
-                );
-              } catch (error) {
-                return null;
+        // block name
+        const block = section.block || section.id || sectionKey
+        switch (block) {
+          default:
+            try {
+
+              if (section.component) {
+                return section.component
               }
-          }
-        })}
+
+              const DynamicBlock = await getThemeBlock(block)
+
+              // console.log('333 :>> ', sectionKey);
+              return (
+                <DynamicBlock
+                  key={sectionKey}
+                  section={section}
+                  {...(data || section.data || {})}
+                />
+              )
+            } catch (error) {
+              return null
+            }
+        }
+      })}
     </>
-  );
+  )
 }
